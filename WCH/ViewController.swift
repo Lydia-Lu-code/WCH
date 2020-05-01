9e46b48e34f2f790e4594dd2fac6a35fab221fc9
Lydia-Lu-code <guipopo5@gmail.com>
2020-05-01 18:08:08 +0800

    end

--- WCH/ViewController.swift
+++ WCH/ViewController.swift
@@ -8,13 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let renderImage = ["batman","spiderman","superman","thor"]
 
+class ViewController: UIViewController {
+    var number = 0
+
+    @IBOutlet weak var changeImage: UIImageView!
+    @IBOutlet weak var helpSegment: UISegmentedControl!
+    @IBOutlet weak var allButton: UIButton!
+    @IBOutlet weak var anyButton: UIButton!
+    
+    @IBOutlet weak var gnomeButton: UIButton!
+    @IBOutlet weak var indexImageView: UIImageView!
+    @IBOutlet weak var blackImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeImage.image = UIImage(named: renderImage[number])
+        helpSegment.selectedSegmentIndex = number
+        
     }
-
-
+    
+    @IBAction func segmentChange(_ sender: UISegmentedControl) {
+        number = Int(sender.selectedSegmentIndex)
+        changeImage.image = UIImage(named: renderImage[number])
+    }
+    
+    @IBAction func renButton(_ sender: UIButton) {
+        number = Int.random(in: 0...renderImage.count-1)
+        helpSegment.selectedSegmentIndex = number
+        changeImage.image = UIImage(named: renderImage[number])
+    }
+    
+    @IBAction func allButton(_ sender: UIButton) {
+        changeImage.image = UIImage(named: "all.png")
+    }
+    
+    @IBAction func gnomeButton(_ sender: UIButton) {
+        indexImageView.addSubview(blackImageView)
+    }
 }
+    
+
+    
+    
+    
+    
+
+
+
+
 
